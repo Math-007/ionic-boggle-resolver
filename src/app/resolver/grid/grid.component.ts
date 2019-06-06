b40e408ae9a7ad6ea0be403e9fb0ef2a5c89740d
Mathieu Marchand <mathieu.marchand@polymtl.ca>
2019-06-05 22:22:00 -0400

    add form validation

--- src/app/resolver/grid/grid.component.ts
+++ src/app/resolver/grid/grid.component.ts
@@ -3,7 +3,7 @@ import {GridDataService} from '../../shared/grid-data.service';
 import {BoggleResolverService} from '../../shared/boggle-resolver.service';
 import {GridData} from '../../shared/DTO/grid-data';
 import {WordList} from '../../shared/DTO/word-list';
-import {ModalController} from '@ionic/angular';
+import {AlertController, ModalController} from '@ionic/angular';
 import {ModalComponent} from '../modal/modal.component';
 
 @Component({
@@ -18,12 +18,22 @@ export class GridComponent implements OnInit {
 
   constructor(public girdDataService: GridDataService,
               private boggleResolverService: BoggleResolverService,
-              private modalController: ModalController) {
+              private modalController: ModalController,
+              public alertController: AlertController) {
+    for (let i = 0; i < this.girdDataService.height; i++) {
+      this.gridRowValid[i] = '';
+    }
   }
 
   ngOnInit() {}
 
   public onSearch(): void {
+    if (this.checkIfRowError()) {
+      console.log('show error alert');
+      this.showErrorAlert();
+      return;
+    }
+
     const gridData: GridData = {
       height: this.girdDataService.height,
       width: this.girdDataService.width,
@@ -47,20 +57,36 @@ export class GridComponent implements OnInit {
   }
 
   public onInput(rowNo: number): void {
-
-    console.log(this.gridLetter[rowNo]);
     const lengthValid: boolean = this.gridLetter[rowNo] ? this.gridLetter[rowNo].length === this.girdDataService.width : false;
     if (!lengthValid) {
       this.gridRowValid[rowNo] = `There must have ${this.girdDataService.width} letters`;
       return;
     }
-    const alphaNumChar: boolean = new RegExp(/^[a-z]+$/i).test(this.gridRowValid[rowNo]);
-    console.log(alphaNumChar);
+    const alphaNumChar: boolean = new RegExp(/^[a-z]+$/i).test(this.gridLetter[rowNo]);
     if (!alphaNumChar) {
       this.gridRowValid[rowNo] = 'Must be alphanumeric character';
       return;
     }
     this.gridRowValid[rowNo] = undefined;
+  }
+
+  private showErrorAlert(): void {
+    this.alertController.create({
+      header: 'Alert',
+      subHeader: 'Validation error',
+      message: 'Some row are not valid',
+      buttons: ['Got it !']
+    }).then((alert: HTMLIonAlertElement) => {
+      alert.present();
+    });
+  }
+
+  private checkIfRowError(): boolean {
+    console.log(this.gridRowValid);
+    for (let i = 0; i < this.girdDataService.height; i++) {
+      if (this.gridRowValid[i] !== undefined) { return true; }
+    }
+    return false;
   }
 
 }
