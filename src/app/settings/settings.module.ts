c27e8aa298f10b7084769e251a3049e862644f04
Mathieu Marchand <mathieu.marchand@polymtl.ca>
2019-06-05 23:52:27 -0400

    fix invisible settings page

--- src/app/settings/settings.module.ts
+++ src/app/settings/settings.module.ts
@@ -6,9 +6,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
 import { SettingsPage } from './settings.page';
-import {ResolverPageModule} from '../resolver/resolver.module';
-import {RangePipe} from '../shared/range.pipe';
-import {SharedModule} from '../shared/shared.module';
+import { SharedModule } from '../shared/shared.module';
 
 const routes: Routes = [
   {
@@ -23,7 +21,6 @@ const routes: Routes = [
         FormsModule,
         IonicModule,
         RouterModule.forChild(routes),
-        ResolverPageModule,
         SharedModule
     ],
   declarations: [SettingsPage]
